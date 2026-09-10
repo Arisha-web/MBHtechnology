f1dd9a9ba3d6cc8855ad9b87fa9487c0bc352b50
Arisha-web <az0851691@gmail.com>
2026-09-10 16:32:36 +0500

    Repair and refresh MBH PWA service worker

--- app/sw.js
+++ app/sw.js
@@ -1,1 +1,19 @@
-const CACHE="mbh-services-v6";const ASSETS=["./mbh-app-2026.webmanifest","./mbh-official-icon-2026.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));self.addEventListener("fetch",e=>{ifFnif0);
+const CACHE="mbh-services-v7";
+const ASSETS=["./mbh-app-2026.webmanifest","./mbh-official-icon-2026.png"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+  ]));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).catch(()=>caches.match("./")));
+  }else{
+    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  }
+});
